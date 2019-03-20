20b95ebf846aececd1a70bb84ea09f4a51b2cbb8
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2019-03-20 22:05:52 +0100

    Support copy to folder in copy-to-pi.sh

--- copy-to-pi.sh
+++ copy-to-pi.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 # Copy via SSH when scp is missing.
 source config-copy-to-pi.inc.sh
-echo "echo $(base64 -w0 "$1") | base64 -d > '$2'" | ssh "$SSH_USER"@"$SSH_HOST" bash
 
+SRC="$1"
+DEST="$2"
+
+if [[ ! -f "$SRC" ]]; then
+    echo "$SRC is not a regular file" >&2
+    exit 1
+fi
+
+# Allow copy to folder.
+if [[ "$(echo -n "$DEST" | tail -c1)" = "/" ]]; then
+    DEST="${DEST}/$(basename "$SRC")"
+fi
+
+echo "echo $(base64 -w0 "$SRC") | base64 -d > '$DEST'" | ssh "$SSH_USER"@"$SSH_HOST" bash
+# TODO: change permissions
