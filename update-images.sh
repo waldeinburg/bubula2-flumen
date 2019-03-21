9b8fab919a7e26fa68c8c78a8e68fdde0e04a7b2
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2019-03-21 22:40:59 +0100

    Update images script

---
+++ update-images.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Take an image in org_img, resize, and put in img.
+
+SIZE=800
+ORG_IMG_DIR="org_img"
+IMG_DIR="img"
+SERVER_IMG_DIR="/var/flumen/img"
+
+cd $(dirname "$0") || exit 1
+
+source config-dev.inc.sh
+
+img-web-path () {
+    echo "${IMG_DIR}/$(basename "$1" ".png")-web.png"
+}
+
+create-resized-img () {
+    SRC="$1"
+    DEST=$(img-web-path "$SRC")
+    convert "$SRC" -resize "${SIZE}x${SIZE}" "$DEST"
+}
+
+
+echo "Creating all non-existing resized images ..."
+find "$ORG_IMG_DIR" -name "*.png" | while read f; do
+    dest=$(img-web-path "$f")
+    if [[ ! -f "$dest" ]]; then
+        echo "Resizing ${f}, creating ${dest} ..."
+        create-resized-img "$f"
+    fi
+done
+
+
+echo "Copying all non-existing images to server ..."
+# Don't run ssh-session for each file to check if it exists.
+existing=$(ssh "$SSH_USER"@"$SSH_HOST" "find ${SERVER_IMG_DIR} -name '*.png'")
+
+find "$IMG_DIR" -name "*.png" | while read f; do
+    name=$(basename "$f")
+    name_re=$(echo "$name" | sed -r 's/\./\\./g')
+    if ! echo $existing | grep -q "${SERVER_IMG_DIR}/${name_re}"; then
+        ./copy-to-rpi.sh "$f" "${SERVER_IMG_DIR}/"
+    fi
+done
+
+
+echo "Done!"
